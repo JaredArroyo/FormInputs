4c8cbb595a59a526528b7a7285a371f81f1b981b
Jared Arroyo <jared.arroyoatx@gmail.com>
2018-12-13 23:26:36 +0000

    now checks if the first letter of name is capitalized

--- flask_app.py
+++ flask_app.py
@@ -9,20 +9,23 @@ def main():
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
     name = request.form.get('input_name', '')
-    dropdown = request.form.get('input_dropdown', '')
-    select = request.form.get('input_select', '')
-    freeform = request.form.get('input_freeform', '')
-    return render_template("main_page.html", input_data=dropdown,
-                           output="You're a wizard %s." % name)
+
+    cap_name = name.upper()
+    first_letter_of_name = name[0]
+    first_letter_of_cap_name = cap_name[0]
+    if(first_letter_of_name != first_letter_of_cap_name):
+        return render_template("main_page.html", output="Please re-enter First Name '%s' start with capital letter" % name)
+    else:
+        dropdown = request.form.get('input_dropdown', '')
+        select = request.form.get('input_select', '')
+        freeform = request.form.get('input_freeform', '')
+        return render_template("main_page.html", input_data=dropdown,
+                            output="You're a wizard %s." % name)
 
 
-def dedupe(x):
-    y = []
-    for i in x:
-        if i not in y:
-            y.append(i)
 
-    return y
 
-animals = ['cats','cats','cats','dogs','pandas']
-print(dedupe(animals))
+
+
+
+
